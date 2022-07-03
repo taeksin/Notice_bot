41b431072be4269b259aeef6a5c29702dcd4e49c
taeksin <yoyo2521@naver.com>
2022-07-03 22:57:32 +0900

    first commit

---
+++ module/notice_module.py
@@ -0,0 +1,288 @@
+import re
+import time
+import logging
+import requests
+import jwt
+import uuid
+import hashlib
+import math
+import os
+import pandas as pd
+import numpy
+import telegram
+from urllib.parse import urlencode
+from decimal import Decimal
+from datetime import datetime
+from ast import literal_eval
+
+# Telegram Keys
+telegram_token = '5599430717:AAEUS_DsOPeL1k3nHoR8VzKAbT3BfE1GvC8'
+telegram_id = '-1001609089896'
+
+# -----------------------------------------------------------------------------
+# - Name : set_loglevel
+# - Desc : 로그레벨 설정
+# - Input
+#   1) level : 로그레벨
+#     1. D(d) : DEBUG              ① DEBUG : 상세한 로그를 보고 싶은 경우
+#     2. E(e) : ERROR              ② INFO : 정보성 로그를 보고 싶은 경우
+#     3. 그외(기본) : INFO          ③ ERROR : 에러 로그를 보고 싶은 경우
+# - Output
+# -----------------------------------------------------------------------------
+
+
+def set_loglevel(level):
+    try:
+
+        # ---------------------------------------------------------------------
+        # 로그레벨 : DEBUG
+        # ---------------------------------------------------------------------
+        if level.upper() == "D":
+            logging.basicConfig(
+                format='[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d]:%(message)s',
+                datefmt='%Y/%m/%d %I:%M:%S %p',
+                level=logging.DEBUG
+            )
+        # ---------------------------------------------------------------------
+        # 로그레벨 : ERROR
+        # ---------------------------------------------------------------------
+        elif level.upper() == "E":
+            logging.basicConfig(
+                format='[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d]:%(message)s',
+                datefmt='%Y/%m/%d %I:%M:%S %p',
+                level=logging.ERROR
+            )
+        # ---------------------------------------------------------------------
+        # 로그레벨 : INFO
+        # ---------------------------------------------------------------------
+        else:
+            # -----------------------------------------------------------------------------
+            # 로깅 설정
+            # 로그레벨(DEBUG, INFO, WARNING, ERROR, CRITICAL)
+            # -----------------------------------------------------------------------------
+            logging.basicConfig(
+                format='[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d]:%(message)s',
+                datefmt='%Y/%m/%d %I:%M:%S %p',
+                level=logging.INFO
+            )
+
+    # ----------------------------------------
+    # Exception Raise
+    # ----------------------------------------
+    except Exception:
+        raise
+
+
+# -----------------------------------------------------------------------------
+# - Name : send_telegram_msg
+# - Desc : 텔레그램 메세지 전송
+# - Input
+#   1) message : 메세지
+# -----------------------------------------------------------------------------
+def send_telegram_message(message):
+    try:
+        # 텔레그램 메세지 발송
+        bot = telegram.Bot(telegram_token)
+        res = bot.sendMessage(chat_id=telegram_id, text=message)
+ 
+        return res
+ 
+    # ----------------------------------------
+    # 모든 함수의 공통 부분(Exception 처리)
+    # ----------------------------------------
+    except Exception:
+        raise
+# -----------------------------------------------------------------------------
+# - Name : send_msg
+# - Desc : 메세지 전송
+# - Input
+#   1) sent_list : 메세지 발송 내역
+#   2) key : 메세지 키
+#   3) contents : 메세지 내용
+#   4) msg_intval : 메세지 발송주기
+# - Output
+#   1) sent_list : 메세지 발송 내역
+# -----------------------------------------------------------------------------
+def send_msg(sent_list, key, contents, msg_intval):
+    try:
+
+        # msg_intval = 'N' 이면 메세지 발송하지 않음
+        if msg_intval.upper() != 'N':
+
+            # 발송여부 체크
+            sent_yn = False
+
+            # 발송이력
+            sent_dt = ''
+
+            # 발송내역에 해당 키 존재 시 발송 이력 추출
+            for sent_list_for in sent_list:
+                if key in sent_list_for.values():
+                    sent_yn = True
+                    sent_dt = datetime.strptime(sent_list_for['SENT_DT'], '%Y-%m-%d %H:%M:%S')
+
+            # 기 발송 건
+            if sent_yn:
+
+                logging.info('기존 발송 건')
+
+                # 현재 시간 추출
+                current_dt = datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+
+                # 시간 차이 추출
+                diff = current_dt - sent_dt
+
+                # 발송 시간이 지난 경우에는 메세지 발송
+                if diff.seconds >= int(msg_intval):
+
+                    logging.info('발송 주기 도래 건으로 메시지 발송 처리!')
+
+                    # 메세지 발송
+                    send_telegram_message(contents)
+
+                    # 기존 메시지 발송이력 삭제
+                    for sent_list_for in sent_list[:]:
+                        if key in sent_list_for.values():
+                            sent_list.remove(sent_list_for)
+
+                    # 새로운 발송이력 추가
+                    sent_list.append({'KEY': key, 'SENT_DT': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+
+                else:
+                    logging.info('발송 주기 미 도래 건!')
+
+            # 최초 발송 건
+            else:
+                logging.info('최초 발송 건')
+
+                # 메세지 발송
+                send_telegram_message(contents)
+
+                # 새로운 발송이력 추가
+                sent_list.append({'KEY': key, 'SENT_DT': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+
+        return sent_list
+
+    # ----------------------------------------
+    # 모든 함수의 공통 부분(Exception 처리)
+    # ----------------------------------------
+    except Exception:
+        raise
+# -----------------------------------------------------------------------------
+# - Name : read_file
+# - Desc : 파일 조회
+# - Input
+# 1. name : File Name
+# - Output
+# 1. 변수값
+# -----------------------------------------------------------------------------
+def read_file(name):
+    try:
+
+        file_to_listofdict = []
+
+        path = './conf/' + str(name) + '.txt'
+
+        f = open(path, 'r', encoding='UTF8')
+        lines = f.read().splitlines()
+        f.close()
+
+        for line in lines:
+            txt_to_dict = literal_eval(line)
+            file_to_listofdict.append(txt_to_dict)
+
+        return file_to_listofdict
+
+    # ----------------------------------------
+    # 모든 함수의 공통 부분(Exception 처리)
+    # ----------------------------------------
+    except FileNotFoundError:
+        logging.info('파일이 존재하지 않습니다! 파일을 생성합니다. 파일명[' + str(name) + '.txt]')
+
+        with open(path, "w", encoding="utf-8") as f:
+            f.close()
+
+        logging.info('파일 생성 완료. 파일명[' + str(name) + '.txt]')
+        return None
+
+    except Exception:
+        raise
+
+
+# -----------------------------------------------------------------------------
+# - Name : write_config_append
+# - Desc : 파일 쓰기(추가)
+# - Input
+# 1. name : Config File Name
+# 2. req_val : 변수값
+# - Output
+# -----------------------------------------------------------------------------
+def write_config_append(name, req_val):
+    try:
+
+        # 파일명
+        file_name = './module/' + str(name) + '.txt'
+
+        # 파일에 저장(추가하기)
+        with open(file_name, "a", encoding="utf-8") as f:
+            f.write(str(req_val))
+            f.close()
+
+    # ----------------------------------------
+    # 모든 함수의 공통 부분(Exception 처리)
+    # ----------------------------------------
+    except Exception:
+        raise
+
+# -----------------------------------------------------------------------------
+# - Name : write_config
+# - Desc : 파일 쓰기(새로쓰기)
+# - Input
+# 1. name : Config File Name
+# 2. req_val : 변수값
+# - Output
+# -----------------------------------------------------------------------------
+def write_config(name, req_val):
+    try:
+
+        # 파일명
+        file_name = './module/' + str(name) + '.txt'
+
+        # 파일에 저장
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write(str(req_val))
+            f.write("\n")
+            f.close()
+
+    # ----------------------------------------
+    # 모든 함수의 공통 부분(Exception 처리)
+    # ----------------------------------------
+    except Exception:
+        raise
+# -----------------------------------------------------------------------------
+# - Name : get_env_keyvalue
+# - Desc : 환경변수 읽어오기
+# - Input
+#   1) key : key
+# - Output
+#   1) Value : 키에 대한 값
+# -----------------------------------------------------------------------------
+def get_env_keyvalue(key):
+    try:
+        path = './env/env.txt'
+
+        f = open(path, 'r', encoding='UTF8')
+        line = f.readline()
+        f.close()
+
+        env_dict = literal_eval(line)
+        logging.debug(env_dict)
+
+        return env_dict[key]
+
+    # ----------------------------------------
+    # 모든 함수의 공통 부분(Exception 처리)
+    # ----------------------------------------
+    except Exception:
+        raise
+    
---
+++ 연습.py
@@ -0,0 +1,195 @@
+import feedparser
+import os
+import sys
+import time
+import datetime
+from decimal import Decimal
+from datetime import timedelta
+from datetime import datetime
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from module import notice_module
+# ┌ ┐└ ┘
+urls='https://www.hansung.ac.kr/bbs/hansung/143/rssList.do?row=50'
+d=feedparser.parse(urls)
+pub_time=[]
+title=[]
+link=[]
+def make_list(pub_time,title,link):
+    # ┌ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ리스트 새로고침ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┐
+    for _ in range(10):
+        pub_time.append(cvt_datetime(d.entries[_].published))
+        title.append(d.entries[_].title)
+        link.append(d.entries[_].link)
+    # print(f'pub_time {pub_time}\n')
+    # print(f'title은 {title}\n')
+    # print(f'link은 {link}\n')
+    # └ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ리스트 새로고침ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘
+def cvt_datetime(obj):
+    obj_time=datetime.strptime(obj, "%Y-%m-%d %H:%M:%S.%f")
+    return obj_time
+
+# 프로그램 시작 메세지 발송
+message = '\n\n[🟣🟣 시작 안내 🟣🟣]'
+message = message + '\n\n notice_bot 시작! '
+message = message + '\n\n- 현재시간:' + str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+
+# 프로그램 시작 메세지 발송
+notice_module.send_telegram_message(message)
+
+# if문에서 시간 비교할때 사용해야함
+std_post_time= d.entries[3].published    #코드 다짜면 생각좀 해보자 0으로 할지 1로할지
+std_post_time=cvt_datetime(std_post_time)
+#print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+make_list(pub_time,title,link)      # 리스트 새로고침
+#print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+now = datetime.now()
+while True:
+    try:
+        d=feedparser.parse(urls)    # 주소 새로고침해서 가져오기
+        make_list(pub_time,title,link)      # 리스트 새로고침
+        
+        # ┌ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 시 간 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┐
+        now = datetime.now()
+        recent_post_time= d.entries[0].published
+        recent_post_time=cvt_datetime(recent_post_time)
+        # NEW POST 등록 된 경우
+        #print(f'if문은 {std_post_time < recent_post_time < now}')
+        if std_post_time.timestamp < recent_post_time.timestamp < now.timestamp:
+            # for문 안에서 new_post가 몇개인지 분석하기 인덱스번호 추출
+            num=pub_time.index(std_post_time)
+            #print("공지가 등록되었습니다. 메시지 보낼게요!")
+            for i in reversed(range(num)):
+                #print("ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ")
+                #print(title[i])
+                #print(link[i])
+                #print(pub_time[i])
+                #print("ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ")
+                message = '\n\n[🔴📝 새로운 공지사항 📝🔴]'
+                message = message +'\n\n['+title[i]+']'
+                message = message + '\n['+link[i]+']'
+                notice_module.send_telegram_message(message)
+            # 텔레그램 전송
+            std_post_time=recent_post_time
+        else:
+            #print("새로운 공지가 없음")
+            time.sleep(1)
+        time.sleep(300)   # 코드 완성하면 300으로 바꾸기
+        # └ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 시 간 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘
+        
+    except KeyboardInterrupt:
+        # 프로그램 종료 메세지 조립
+        message = '\n\n[🚨❌🚨종료🚨❌🚨]'
+        message = message + '\n\n notice_bot 종료!'
+        message = message + '\n\n KeyboardInterrupt Exception 발생!'
+        message = message + '\n\n- 현재시간:' + str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+        
+        # # 프로그램 종료 메세지 발송
+        notice_module.send_telegram_message(message)
+        # logging.error("KeyboardInterrupt Exception 발생!")
+        # logging.error(traceback.format_exc())
+        sys.exit(-100)
+
+    except Exception:
+        # 프로그램 종료 메세지 조립
+        message = '\n\n[🚨❌🚨종료🚨❌🚨]'
+        message = message + '\n\n notice_bot 종료!'
+        message = message + '\n\n Exception 발생!'
+        message = message + '\n\n- 현재시간:' + str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+        
+        # # 프로그램 종료 메세지 발송
+        notice_module.send_telegram_message(message)
+        # logging.error("Exception 발생!")
+        # logging.error(traceback.format_exc())
+        sys.exit(-200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import requests
+# import sys
+# import os
+# from bs4 import BeautifulSoup
+# # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# # from module import notice_module
+
+# keyword="비교과"
+# url="https://www.hansung.ac.kr/hansung/8385/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGaGFuc3VuZyUyRjE0MyUyRmFydGNsTGlzdC5kbyUzRmJic0NsU2VxJTNEMjM5JTI2YmJzT3BlbldyZFNlcSUzRCUyNmlzVmlld01pbmUlM0RmYWxzZSUyNnNyY2hDb2x1bW4lM0RzaiUyNnNyY2hXcmQlM0QlMjY%3D"
+# # 기준 숫자
+# standard_num=1124
+# r=requests.get(url)
+# bs=BeautifulSoup(r.content, "html.parser")
+# #atags=bs.select("td.td-num,a.strong")
+# #atags=bs.select("strong") #제목 부르기는 strong태그의 text부르기
+# trs=bs.select("tr")
+# print(trs)
+# tdn=bs.select("td.td-num") # td-num 리스트
+# tds=bs.select("td.td-subject") # td-subject 리스트
+# trs.find_all('span').decompose()
+# print(trs[0])
+# # 새로고침
+# print(tdn[0])
+# # for td in tdn:
+# #     nnum=td.text
+# #     print(nnum)
+# #     print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n")
+# # print(nnum)
+
+
+
