96071ad97f55298540f06004d5b53aff311a85e7
taeksin <yoyo2521@naver.com>
2022-07-11 21:35:02 +0900

    알림봇 완성?

--- practice.py
+++ practice.py
@@ -14,16 +14,15 @@ d=feedparser.parse(urls)
 pub_time=[]
 title=[]
 link=[]
-def make_list(pub_time,title,link):
+def make_list(pub_time1,title1,link1,d1):
+    d1=feedparser.parse(urls)
     # ┌ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ리스트 새로고침ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┐
     for _ in range(10):
-        pub_time.append(cvt_datetime(d.entries[_].published))
-        title.append(d.entries[_].title)
-        link.append(d.entries[_].link)
-    # print(f'pub_time {pub_time}\n')
-    # print(f'title은 {title}\n')
-    # print(f'link은 {link}\n')
+        pub_time1.append(cvt_datetime(d1.entries[_].published))
+        title1.append(d1.entries[_].title)
+        link1.append(d1.entries[_].link)
     # └ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ리스트 새로고침ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘
+    return pub_time1, title1, link1
 def cvt_datetime(obj):
     obj_time=datetime.strptime(obj, "%Y-%m-%d %H:%M:%S.%f")
     return obj_time
@@ -39,20 +38,20 @@ notice_module.send_telegram_message(message)
 std_post_time= d.entries[3].published    #코드 다짜면 생각좀 해보자 0으로 할지 1로할지
 std_post_time=cvt_datetime(std_post_time)
 print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-make_list(pub_time,title,link)      # 리스트 새로고침
+pub_time,title,link=make_list(pub_time,title,link,d)      # 리스트 새로고침
 print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
 now = datetime.now()
 while True:
     try:
         d=feedparser.parse(urls)    # 주소 새로고침해서 가져오기
-        make_list(pub_time,title,link)      # 리스트 새로고침
+        pub_time,title,link=make_list(pub_time,title,link,d)      # 리스트 새로고침
         
         # ┌ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 시 간 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┐
         now = datetime.now()
         recent_post_time= d.entries[0].published
         recent_post_time=cvt_datetime(recent_post_time)
         # NEW POST 등록 된 경우
-        if std_post_time < recent_post_time < now:
+        if std_post_time.timestamp() < recent_post_time.timestamp() < now.timestamp():
             # for문 안에서 new_post가 몇개인지 분석하기 인덱스번호 추출
             num=pub_time.index(std_post_time)
             for i in reversed(range(num)):
@@ -64,7 +63,8 @@ while True:
             # 텔레그램 전송
             std_post_time=recent_post_time
         else:
-            print("새로운 공지가 없음")
+            print("새로운 공지가 없음 - "+str(now.strftime('%Y-%m-%d %H:%M:%S')))
+
         time.sleep(300)   # 코드 완성하면 300으로 바꾸기
         # └ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 시 간 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘
         
