f18d1b4b1a9f73b6deb9246949c5229fa4bdeb5e
Taeksin Kim <yoyo2521@naver.com>
2023-12-13 21:21:48 +0900

    reset

--- 1test.py
+++
@@ -1,3 +0,0 @@
-my_dict = {'key': [1,2,3]}
-last_value = my_dict['key'][2]
-print(last_value)
--- notice.py
+++
@@ -1,5 +0,0 @@
-class Notice:
-    def __init__(self, id: str, title: str, url: str):
-        self.id = id
-        self.title = title
-        self.url = url
